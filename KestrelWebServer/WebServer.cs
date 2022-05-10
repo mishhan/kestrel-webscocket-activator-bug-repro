0789ecd722449364211251f09e1cc0ee0f21531f
Mikhail <misha.smi1996@yandex.ru>
2022-05-10 14:18:57 +0500

    WebServer using Activator

---
+++ KestrelWebServer/WebServer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace KestrelWebServer
+{
+    public class WebServer
+    {
+        public WebServer()
+        {
+			/* for simplicity everything in constructor */
+			var httpUrl = @"http://127.0.0.1:8008/";
+			var hostBuilder = new WebHostBuilder()
+				.UseKestrel()
+				.UseUrls(httpUrl);
+
+			hostBuilder.Configure(app =>
+			{
+				app.UseWebSockets();
+				app.Run(async context =>
+				{
+					if (!context.WebSockets.IsWebSocketRequest)
+					{
+						await context.Response.WriteAsync("Not WebSocketRequest");
+					}
+					else
+					{
+						try
+						{
+							await context.WebSockets.AcceptWebSocketAsync();
+							Console.WriteLine("Connected");
+						}
+						catch (Exception e)
+						{
+							Console.WriteLine(e);
+							throw;
+						}
+					}
+				});
+			});
+
+			var host = hostBuilder.Build();
+			host.Start();
+		}
+    }
+}
---
+++ KestrelWebSocketActivator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.WebSockets;
+using System.Reflection;
+
+var assembly = Assembly.LoadFrom("KestrelWebServer.dll");
+Activator.CreateInstance(assembly.ExportedTypes.First());
+
+var webSocketClient = new ClientWebSocket();
+var token = new CancellationToken();
+try
+{
+	await webSocketClient.ConnectAsync(new Uri("ws://127.0.0.1:8008/"), token);
+}
+catch (Exception e)
+{
+	Console.WriteLine(e);
+	throw;
+}
